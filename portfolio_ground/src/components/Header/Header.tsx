618d3d5c12b7b4aada939ae42652c06033749a99
Denys Pyshniuk <denys.pyshniuk@gmail.com>
2023-08-17 22:56:39 -0700

    Create NavBar component

--- portfolio_ground/src/components/Header/Header.tsx
+++ portfolio_ground/src/components/Header/Header.tsx
@@ -1,8 +1,10 @@
+import NavTab from "./NavTab";
 
 export default function Header() {
   return (
     <div>
       <h1>Header...</h1>
+      <NavTab/>
     </div>
   );
 }
---
+++ portfolio_ground/src/components/Header/NavTab.tsx
@@ -0,0 +1,9 @@
+export default function NavTab() {
+  return (
+    <ul>
+      <li>Home</li>
+      <li>About</li>
+      <li>Contacts</li>
+    </ul>
+  )
+}
